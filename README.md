055764447f0596b1a98b00686e2f433af119718a
Jaegar Sarauer <jaegars@hotmail.com>
2018-06-05 16:31:49 -0700

    NPM setup & documentation and readme setup.

---
+++ README.md
@@ -0,0 +1,4 @@
+# Seed Engine
+
+### Welcome
+Seed Engine is a lightweight 2D game engine using WebGL2. The engine is designed on the focus of creating a bridge between creating and publishing games to the Seed Network as modules.
---
+++ src/Engine.js
@@ -0,0 +1,43 @@
+'use-strict';
+
+/**
+ * Engine Singleton class. Entry point reference to access managers and
+ * to start and stop the Engine.
+ */
+export class _Engine {
+    /**
+     * Engine Contstructor.
+     */
+    constructor() {
+    }
+
+    /**
+     * Starts the engine.
+     */
+    start() {
+    }
+
+    /**
+     * Stops the engine and cleans up code.
+     */
+    end() {
+    }
+
+    /**
+     * Pauses the entire engine.
+     */
+    pause() {
+    }
+
+    /**
+     * Resumes to the Engine.
+     */
+    unpause() {
+    }
+    
+}
+
+/**
+ * Engine Singleton reference.
+ */
+export default Engine = new _Engine();
