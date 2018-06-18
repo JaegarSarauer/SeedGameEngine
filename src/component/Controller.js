166a4ddba4194b76f2fa598ff54bd0bf381f156a
Jaegar Sarauer <jaegars@hotmail.com>
2018-06-18 11:52:21 -0700

    Controllers updated to match naming conventions of how they are used. Controllers no longer create additional callbacks to manage.

--- src/component/Controller.js
+++ src/component/Controller.js
@@ -3,55 +3,12 @@ import KeyCode from '../const/KeyCode';
 import InputManager from '../manager/InputManager';
 
 /**
- * A Collider Component that checks bounds only on a plane.
- * 
- * This commponent has interface functions for calculating collisions
- * which have to be defined to work. See Subclasses.
+ * A Baseclass for controller components. It is advised to use
+ * controllers to contain movement logic. There are basic movement and 
+ * onClick controllers. You may make your own, remember to derive this class.
  */
 export default class Controller extends Component {
     constructor() {
-        super(true);
-        this.KeyDownCallbacks = {};
-        this.KeyPressedCallbacks = {};
-        this.KeyUpCallbacks = {};
-    }
-
-    assignOnKeyDown(KeyCode, callback) {
-        this.KeyDownCallbacks[KeyCode] = callback;
-    }
-
-    assignOnKeyPressed(KeyCode, callback) {
-        this.KeyPressedCallbacks[KeyCode] = callback;
-    }
-
-    assignOnKeyUp(KeyCode, callback) {
-        this.KeyUpCallbacks[KeyCode] = callback;
-    }
-
-    triggerKeyCallbacks() {
-        //down keys
-        let downKeys = Object.keys(this.KeyDownCallbacks);
-        for (let i = 0; i < downKeys.length; i++) {
-            if (InputManager.isKeyDown(downKeys[i]))
-                this.KeyDownCallbacks[downKeys[i]]();
-        }
-
-        //pressed keys
-        let pressedKeys = Object.keys(this.KeyPressedCallbacks);
-        for (let i = 0; i < pressedKeys.length; i++) {
-            if (InputManager.isKeyPressed(pressedKeys[i]))
-                this.KeyPressedCallbacks[pressedKeys[i]]();
-        }
-
-        //up keys
-        let upKeys = Object.keys(this.KeyUpCallbacks);
-        for (let i = 0; i < upKeys.length; i++) {
-            if (InputManager.isKeyUp(upKeys[i]))
-                this.KeyUpCallbacks[upKeys[i]]();
-        }
-    }
-
-    onUpdate() {
-        this.triggerKeyCallbacks();
+        super(false);
     }
 }
