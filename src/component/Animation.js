19e1a8f3ea4fca05517ff98b2d58107c44812575
Jaegar Sarauer <jaegars@hotmail.com>
2018-06-24 11:23:00 -0700

    Animations changed to allow custom animations with steps and final callback.

--- src/component/Animation.js
+++ src/component/Animation.js
@@ -5,6 +5,11 @@ export default class Animation extends Component {
         super(false);
         this.className = 'Animation';
         this.interpolationData = {};
+        this.animationFrames = 0;
+    }
+
+    animationFramesLeft() {
+        return this.animationFrames;
     }
 
     isAnimating(interpol = null) {
@@ -13,57 +18,60 @@ export default class Animation extends Component {
         return this.interpolationData[interpol] != null;
     }
 
-    interpolatePosition(posX = 0, posY = 0, posZ = 0, frames = 1) {
-        this.interpolationData.position = {
-            frameChange: () => {
-                this.gameObject.transform.translate(posX, posY, posZ);
-            },
-            finalFrame: () => {
-                let start = this.interpolationData.position.startPosition;
-                this.gameObject.transform.setPosition(start.x + posX * frames, start.y + posY * frames, start.z + posZ * frames);
-            },
-            startPosition: this.gameObject.transform.getPosition(),
+    _buildInterpolation(key, frames, frameChange, finalFrame) {
+        this.interpolationData[key] = {
+            frameChange,
+            finalFrame,
             framesLeft: frames,
         };
     }
 
-    interpolateScale(scaleX = 0, scaleY = 0, scaleZ = 0, frames = 1) {
-        this.interpolationData.scale = {
-            frameChange: () => {
-                this.gameObject.transform.scale(scaleX, scaleY, scaleZ);
-            },
-            finalFrame: () => {
-                let start = this.interpolationData.scale.startScale;
-                this.gameObject.transform.setScale(start.x + scaleX * frames, start.y + scaleY * frames, start.z + scaleZ * frames);
-            },
-            startScale: this.gameObject.transform.getScale(),
-            framesLeft: frames,
-        };
+    interpolatePosition(posX = 0, posY = 0, posZ = 0, frames = 1, cycles = 1, reset = false) {
+        let start = this.gameObject.transform.getPosition();
+        this._buildInterpolation('position', frames, () => {
+            this.gameObject.transform.translate(posX, posY, posZ);
+        }, () => {
+            this.gameObject.transform.setPosition(start.x + (reset ? 0 : posX * frames), start.y + (reset ? 0 : posY * frames), start.z + (reset ? 0 : posZ * frames));
+            if (--cycles > 0)
+                this.interpolatePosition(posX, posY, posZ, frames, cycles, reset);
+        })
     }
 
-    interpolateRotation(rotate, frames = 1) {
-        this.interpolationData.rotation = {
-            frameChange: () => {
-                this.gameObject.transform.rotate(rotate);
-            },
-            finalFrame: () => {
-                let start = this.interpolationData.rotation.startRotation;
-                this.gameObject.transform.setRotation(start + rotate * frames);
-            },
-            startRotation: this.gameObject.transform.getRotation(),
-            framesLeft: frames,
-        };
+    interpolateScale(scaleX = 0, scaleY = 0, scaleZ = 0, frames = 1, cycles = 1, reset = false) {
+        let start = this.gameObject.transform.getScale();
+        this._buildInterpolation('scale', frames, () => {
+            this.gameObject.transform.scale(scaleX, scaleY, scaleZ);
+        },() => {
+            this.gameObject.transform.setScale(start.x + (reset ? 0 : scaleX * frames), start.y + (reset ? 0 : scaleY * frames), start.z + (reset ? 0 : scaleZ * frames));
+            if (--cycles > 0)
+                this.interpolateScale(scaleX, scaleY, scaleZ, frames, cycles, reset);
+        });
+    }
+
+    interpolateRotation(rotate, frames = 1, cycles = 1, reset = false) {
+        let start = this.gameObject.transform.getRotation();
+        this._buildInterpolation('rotation', frames, () => {
+            this.gameObject.transform.rotate(rotate);
+        },() => {
+            this.gameObject.transform.setRotation(start + (reset ? 0 : rotate * frames));
+            if (--cycles > 0)
+                this.interpolateRotation(rotate, frames, cycles, reset);
+        });
     }
 
     onUpdate() {
+        this.animationFrames--;
         let interpols = Object.keys(this.interpolationData);
         for (let i = 0; i < interpols.length; i++) {
             let interpol = this.interpolationData[interpols];
+            this.animationFrames = Math.max(this.animationFrames, interpol.framesLeft - 1);
             if (interpol.framesLeft > 1) {
                 interpol.frameChange();
                 interpol.framesLeft--;
-            } else {
+            } else if (interpol.framesLeft > 0) {
                 interpol.finalFrame();
+                interpol.framesLeft--;
+            } else if (interpol.framesLeft <= 0) {
                 delete this.interpolationData[interpols];
             }
         }
